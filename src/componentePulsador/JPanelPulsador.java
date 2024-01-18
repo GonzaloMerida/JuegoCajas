19b1c92e9e327041903dcc1ba1243b41acc1d382
Gonzalo <gonzalomerida@hotmail.com>
2024-01-18 09:57:09 +0100

    x

---
+++ src/componentePulsador/JPanelPulsador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package componentePulsador;
+
+import java.io.File;
+import java.io.Serializable;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class JPanelPulsador extends JPanel implements Serializable{
+    private File rutaImagen;
+    private static PulsadorListener pulsadorListener;
+}
